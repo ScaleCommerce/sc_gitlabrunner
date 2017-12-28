102070ce9c7f8643993a34ba27c18080877c6b26
Andreas Ziethen <az@scale.sc>
2017-12-28 17:09:35 +0100

    add supervisor class

---
+++ manifests/supervisor.pp
@@ -0,0 +1,13 @@
+class sc_gitlabrunner::supervisor(
+  $supervisor_exec_path   = '/usr/local/bin',
+){
+  include supervisord
+
+  supervisord::program { 'gitlab-runner':
+    command     => "/usr/bin/gitlab-ci-multi-runner run --working-directory /home/gitlab-runner --config /etc/gitlab-runner/config.toml --service gitlab-runner --syslog --user gitlab-runner",
+    autostart   => true,
+    autorestart => true,
+    require     => Package['gitlab-ci-multi-runner'],
+    before      => Service['gitlab-runner'],
+  }
+}
