b538eb61fa3827ffce93c603def7816db6d17e69
Andreas Ziethen <az@scale.sc>
2017-12-28 17:09:01 +0100

    remove quotes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,4 @@
 class sc_gitlabrunner (
-  $gitlab_ci_runners = {},
   $use_supervisor = true,
 ){
 
@@ -7,6 +6,6 @@ class sc_gitlabrunner (
     class {'::sc_gitlabrunner::supervisor':}
   }
 
-  include "::gitlab_ci_multi_runner"
-  include "::gitlab_ci_multi_runner::install"
+  include gitlab_ci_multi_runner
+  include gitlab_ci_multi_runner::install
 }
