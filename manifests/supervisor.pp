f8a1c5b9d7cbd89d07214aae99e872b712cd5eef
Andreas Ziethen <az@scale.sc>
2018-01-11 13:18:20 +0100

    add exec to stop service before it's started via supervisor

--- manifests/supervisor.pp
+++ manifests/supervisor.pp
@@ -3,6 +3,10 @@ class sc_gitlabrunner::supervisor(
 ){
   include supervisord
 
+  exec { 'stop-gitlab-runner-service':
+    command => '/usr/bin/gitlab-runner stop',
+    require     => Exec['gitlab-runner-restart'],
+  }->
   supervisord::program { 'gitlab-runner':
     command     => "/usr/bin/gitlab-runner run --working-directory /home/gitlab-runner --config /etc/gitlab-runner/config.toml --service gitlab-runner --syslog --user gitlab-runner",
     autostart   => true,
