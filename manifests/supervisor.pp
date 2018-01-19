7420d1d1890991df51656c656624731c2473ccb8
Andreas Ziethen <az@scale.sc>
2018-01-19 18:06:07 +0100

    add test to check if more then one gitlab-runner processes are running

--- manifests/supervisor.pp
+++ manifests/supervisor.pp
@@ -3,14 +3,16 @@ class sc_gitlabrunner::supervisor(
 ){
   include supervisord
 
-  exec { 'stop-gitlab-runner-service':
-    command => '/usr/bin/gitlab-runner stop',
-    require     => Exec['gitlab-runner-restart'],
-  }->
   supervisord::program { 'gitlab-runner':
     command     => "/usr/bin/gitlab-runner run --working-directory /home/gitlab-runner --config /etc/gitlab-runner/config.toml --service gitlab-runner --syslog --user gitlab-runner",
     autostart   => true,
     autorestart => true,
     require     => Exec['gitlab-runner-restart'],
+  }->
+
+  exec { 'stop-gitlab-runner-service':
+    command => '/usr/bin/gitlab-runner stop',
+    require     => Exec['gitlab-runner-restart'],
+    onlyif  => [ '/usr/bin/test `/usr/bin/pgrep -c gitlab-runner` -gt 1' ],
   }
 }
